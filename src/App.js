5d2b46423c531d3839f015237c6d8e875e932f19
mahadihasan2 <mahadihasan125643@gmail.com>
2022-03-16 18:48:34 +0600

    concept-update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+  const products = [
+    { name: 'Laptop', price: '200000' },
+    { name: 'Phone', price: '20000' },
+    { name: 'Watch', price: '2000' },
+    { name: 'Tablet', price: '22000' },
+    { name: 'AirPhone', price: '1000' },
+  ]
   return (
     <div className="App">
-      <Product name="Laptop" price="200000"></Product>
-      <Product name="Phone" price="20000"></Product>
+      {/* <Product name="Laptop" price="200000"></Product>
+      <Product name="Phone" price="20000"></Product> */}
+      {
+        products.map(product => <Product name={product.name} price={product.price}></Product>)
+      }
 
     </div>
   );
