32f0a0ff156cacfc287039adbece60329d70ff7a
mahadihasan2 <mahadihasan125643@gmail.com>
2022-03-16 18:39:30 +0600

    React-core-concept

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Product name="Laptop" price="200000"></Product>
+      <Product name="Phone" price="20000"></Product>
+
     </div>
   );
+}
+function Product(props) {
+  return (
+    <div className='product'>
+      <h1>Name: {props.name} </h1>
+      <h2>Price: {props.price}</h2>
+    </div>
+  )
 }
 
 export default App;
